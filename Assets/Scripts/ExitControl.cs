81df44d3b5a9747f37773b39ac4b3e9a72005221
Petr Manas <peter@petermanas.com>
2017-10-28 23:04:49 +0100

    alien and stuff

---
+++ Assets/Scripts/ExitControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitControl : MonoBehaviour {
+
+	public string nextLevel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		Destroy (other.gameObject);
+		if (other.tag == "President") {
+			Destroy (gameObject);
+			SceneManager.LoadScene (nextLevel);
+		}
+	}
+
+}
