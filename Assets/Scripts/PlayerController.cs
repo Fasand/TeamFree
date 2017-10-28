a73fbc7e68756044a789e0c2700d910f714cb246
Narrik <kristiankalinak@gmail.com>
2017-10-28 17:34:44 +0100

    hhkhhkhk

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
 	public float speed;             //Floating point variable to store the player's movement speed.
 
 	private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+	private bool turnedR;
+	private bool notMoving;
 
 	// Use this for initialization
 	void Start()
@@ -19,24 +21,78 @@ public class PlayerController : MonoBehaviour {
 	{
 		Vector2 velocity = rb2d.velocity;
 
+		notMoving = true;
+
 		if (Input.GetKey("up"))
 		{
 			velocity.y = speed;
+			velocity.x = 0;
+			notMoving = false;
 		}
-		else if (Input.GetKey("down"))
+		if (Input.GetKey("down"))
 		{
-			velocity.y = speed * -1;
+			velocity.y = -speed;
+			velocity.x = 0;
+			notMoving = false;
 		}
 
-		else if (Input.GetKey("right"))
+		if (Input.GetKey("right"))
 		{
+			if (!turnedR) {
+				turnedR = !turnedR;
+			}
+			velocity.y = 0;
 			velocity.x = speed;
+			notMoving = false;
 		}
-		else if (Input.GetKey("left"))
+		if (Input.GetKey("left"))
 		{
-			velocity.x = speed * -1;
+			if (turnedR) {
+				turnedR = !turnedR;
+			}
+			velocity.y = 0;
+			velocity.x = -speed;
+			notMoving = false;
 		}
-		else
+		if (Input.GetKey("up") && Input.GetKey("right"))
+		{
+			if (!turnedR) {
+				turnedR = !turnedR;
+			}
+			velocity.y = speed*0.75f;
+			velocity.x = speed*0.75f;
+			notMoving = false;
+		}
+		if (Input.GetKey("up") && Input.GetKey("left"))
+		{
+			if (turnedR) {
+				turnedR = !turnedR;
+			}
+			velocity.y = speed*0.75f;
+			velocity.x = -speed*0.75f;
+			notMoving = false;
+		}
+
+		if (Input.GetKey("down") && Input.GetKey("right"))
+		{
+			if (!turnedR) {
+				turnedR = !turnedR;
+			}
+			velocity.y = -speed*0.75f;
+			velocity.x = speed*0.75f;
+			notMoving = false;
+		}
+		if (Input.GetKey("down") && Input.GetKey("left"))
+		{
+			if (turnedR) {
+				turnedR = !turnedR;
+			}
+			velocity.y = -speed*0.75f;
+			velocity.x = -speed*0.75f;
+			notMoving = false;
+		}
+
+		if (notMoving)
 		{
 			velocity.y = 0;
 			velocity.x = 0;
