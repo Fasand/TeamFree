0e6f41d7981e50c553fd8bb2a1f87cb8ff54dc9f
Petr Manas <peter@petermanas.com>
2017-10-28 18:25:27 +0100

    changed menu, added enemy and other sprites

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public string targetScene;
+
+	public void changeToScene()
+	{
+		SceneManager.LoadScene (targetScene);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
